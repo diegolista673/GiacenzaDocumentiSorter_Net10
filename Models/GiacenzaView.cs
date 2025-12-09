59b8578cf0c1042f159ec158548b1820ca1fc690
diegolista673 <diegolista673@hotmail.com>
2025-12-09 10:53:53 +0100

    aggiorna

--- Models/GiacenzaView.cs
+++ Models/GiacenzaView.cs
@@ -1,21 +1,14 @@
-﻿using GiacenzaSorterRm.Models.Database;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace GiacenzaSorterRm.Models
 {
     public class GiacenzaView
     {
-        
         public string? Centro { get; set; }
 
-        public string Piattaforma { get; set; }
+        public string? Piattaforma { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:N0}", ApplyFormatInEditMode = true)]
         public int Giacenza { get; set; }
-
     }
 }
